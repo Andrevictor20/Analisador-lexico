13abebfd06f388ef12a09bb0db0fd7724400bb95
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-11-09 18:34:30 +0000

    Ajustes em GenerateAst ajuste na formatação e adição do método accept na classe gerada

--- analisador-lexico/src/main/java/br/com/ferramentas/GenerateAst.java
+++ analisador-lexico/src/main/java/br/com/ferramentas/GenerateAst.java
@@ -32,17 +32,19 @@ public class GenerateAst {
     writer.println("abstract class " + baseName + " {");
     defineVisitor(writer, baseName, types);
 
-     for (String type : types) {
+    for (String type : types) {
       String className = type.split(":")[0].trim();
-      String fields = type.split(":")[1].trim(); 
+      String fields = type.split(":")[1].trim();
       defineType(writer, baseName, className, fields);
     }
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
 
     writer.println("}");
     writer.close();
   }
 
-    private static void defineVisitor(
+  private static void defineVisitor(
       PrintWriter writer, String baseName, List<String> types) {
     writer.println("  interface Visitor<R> {");
 
@@ -55,8 +57,7 @@ public class GenerateAst {
     writer.println("  }");
   }
 
-
-   private static void defineType(
+  private static void defineType(
       PrintWriter writer, String baseName,
       String className, String fieldList) {
     writer.println("  static class " + className + " extends " +
@@ -70,6 +71,13 @@ public class GenerateAst {
       writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" +
+        className + baseName + "(this);");
     writer.println("    }");
 
     writer.println();
