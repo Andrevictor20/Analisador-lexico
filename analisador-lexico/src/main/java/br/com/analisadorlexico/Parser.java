7b5aace606ba80c2f6517f92c22212caa07241b2
ItaloMatheus10 <italomrs2345@gmail.com>
2025-11-13 10:56:00 -0300

    Adição de tratamento ao método primary

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Parser.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Parser.java
@@ -90,6 +90,8 @@ class Parser {
       consume(RIGHT_PAREN, "Expect ')' after expression.");
       return new Expr.Grouping(expr);
     }
+
+    throw error(peek(), "Expect expression.");
   }
 
   private boolean match(TokenType... types) {
