8aab49ab34bc32d864478f42dd02a2d3f952bf9e
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-11-13 00:24:50 +0000

    Criação dos metodos comparison, term e factor no parser

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Parser.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Parser.java
@@ -25,6 +25,41 @@ class Parser {
     
     return expr;
   }
+    private Expr comparison() {
+    Expr expr = term();
+
+    while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = term();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+   private Expr term() {
+    Expr expr = factor();
+
+    while (match(MINUS, PLUS)) {
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+   private Expr factor() {
+    Expr expr = unary();
+
+    while (match(SLASH, STAR)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
   private boolean match(TokenType... types) {
     for (TokenType type : types) {
       if (check(type)) {
@@ -55,4 +90,6 @@ class Parser {
   private Token previous() {
     return tokens.get(current - 1);
   }
+
+  
 }
