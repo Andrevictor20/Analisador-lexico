c3eef6c4a96cb97986658f8de0289a701e21c122
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-11-14 12:54:13 +0000

    Refatora o método visitBinaryExpr para incluir suporte à operação de adição entre strings e números

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Interpreter.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Interpreter.java
@@ -38,19 +38,29 @@ class Interpreter implements Expr.Visitor<Object> {
     }
 
     @Override
-  public Object visitBinaryExpr(Expr.Binary expr) {
-    Object left = evaluate(expr.left);
-    Object right = evaluate(expr.right); 
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
 
-    switch (expr.operator.type) {
-      case MINUS:
-        return (double)left - (double)right;
-      case SLASH:
-        return (double)left / (double)right;
-      case STAR:
-        return (double)left * (double)right;
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double) left - (double) right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+
+                break;
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+        }
+
+        return null;
     }
-
-    return null;
-  }
 }
