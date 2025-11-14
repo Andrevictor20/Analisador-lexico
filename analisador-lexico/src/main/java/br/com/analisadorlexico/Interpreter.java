0233321835f90358dbabb3d82cca55c6c1301964
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-11-14 12:46:36 +0000

    Implementação do método evaluate e correção na visitação de expressões literais

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Interpreter.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Interpreter.java
@@ -1,6 +1,11 @@
 package br.com.analisadorlexico;
 
 class Interpreter implements Expr.Visitor<Object> {
-    
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+    private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
 }
-
