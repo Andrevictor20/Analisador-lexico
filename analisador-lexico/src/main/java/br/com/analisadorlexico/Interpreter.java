2eafd60993a928ff55e39fca2dfd1d6e0d09ee86
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-11-14 12:49:13 +0000

    Refatoração do método evaluate e implementação do visitante para expressões unárias e de agrupamento

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Interpreter.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Interpreter.java
@@ -5,7 +5,25 @@ class Interpreter implements Expr.Visitor<Object> {
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
     }
-    private Object evaluate(Expr expr) {
-    return expr.accept(this);
+
+     @Override
+  public Object visitUnaryExpr(Expr.Unary expr) {
+    Object right = evaluate(expr.right);
+
+    switch (expr.operator.type) {
+      case MINUS:
+        return -(double)right;
+    }
+
+    return null;
   }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
 }
