090d3c7836d22d2a0dd1fb1dedc0a7da2a364de3
ItaloMatheus10 <italomrs2345@gmail.com>
2025-11-20 13:53:30 -0300

    Informe da linha de erro ao usuário

--- analisador-lexico/src/main/java/br/com/analisadorlexico/Lox.java
+++ analisador-lexico/src/main/java/br/com/analisadorlexico/Lox.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class Lox {
 
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +29,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError)
             System.exit(65);
+            if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
